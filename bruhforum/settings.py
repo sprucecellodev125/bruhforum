aff49595bff2c85f9a5d99ee78f789a01f8d2265
sprucecellodev125 <91181632+sprucecellodev125@users.noreply.github.com>
2023-07-18 13:19:19 +0000

    Added .env

--- bruhforum/settings.py
+++ bruhforum/settings.py
@@ -11,6 +11,9 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 
 from pathlib import Path
+import os
+from dotenv import load_dotenv
+load_dotenv()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -78,10 +81,10 @@ WSGI_APPLICATION = 'bruhforum.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'bruhforum',
-        'USER': '',
-        'PASSWORD': '',
-        'HOST': '',
+        'NAME': str(os.getenv('DB_NAME')),
+        'USER': str(os.getenv('DB_USER')),
+        'PASSWORD': str(os.getenv('DB_PASSWORD')),
+        'HOST': str(os.getenv('DB_HOST')),
         'PORT': '3306'
     }
 }
