38b27a69255cff35394497a90b57299efe33d708
sprucecellodev125 <antonijonsianturi@gmail.com>
2023-08-02 14:15:25 +0000

    Added Navbar on frontend

--- frontend/src/routes/Homepage.js
+++ frontend/src/routes/Homepage.js
@@ -1,27 +1,27 @@
 import 'bootstrap/dist/css/bootstrap.min.css';
-import Button from 'react-bootstrap/Button';
 import Nav from 'react-bootstrap/Nav';
+import Navbar from 'react-bootstrap/Navbar';
 import Container from 'react-bootstrap/Container';
-import Row from 'react-bootstrap/Row';
-import Col from 'react-bootstrap/Col';
 
 function Homepage() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <Navbar expand="lg" style={{backgroundColor: `#e3f2fd`}}>
+        <Container fluid>
+          <Navbar.Brand href="/">Bruhforum</Navbar.Brand>
+          <Navbar.Toggle aria-controls="basic-navbar-nav" />
+          <Navbar.Collapse id="basic-navbar-nav">
+            <Nav className="me-auto">
+              <Nav.Link href="/create">Create</Nav.Link>
+              <Nav.Link href="#overview">Forum settings</Nav.Link>
+            </Nav>
+          </Navbar.Collapse>
+        </Container>
+      </Navbar>
+      <Container>
+        <h1>Hello world</h1>
+      </Container>
+    </>
   );
 }
 
